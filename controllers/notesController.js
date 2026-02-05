d8a5b5a59876056344945fb27b3199d3a971f8da
unfit-gokul007 <gokulkathick984@gmail.com>
2026-02-05 11:30:21 +0530

    Fix noteController import issue

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,32 +1,25 @@
 import Note from "../models/Note.js";
 
-// CREATE NOTE
+// Create note
 export const createNote = async (req, res) => {
   try {
-    const { title, content } = req.body;
-
-    if (!title || !content) {
-      return res.status(400).json({ message: "All fields required" });
-    }
-
     const note = await Note.create({
-      title,
-      content,
       user: req.user,
+      title: req.body.title,
+      content: req.body.content,
     });
-
     res.status(201).json(note);
   } catch (error) {
-    res.status(500).json({ message: "Failed to create note" });
+    res.status(500).json({ message: error.message });
   }
 };
 
-// GET USER NOTES
+// Get notes
 export const getNotes = async (req, res) => {
   try {
-    const notes = await Note.find({ user: req.user }).sort({ createdAt: -1 });
+    const notes = await Note.find({ user: req.user });
     res.json(notes);
   } catch (error) {
-    res.status(500).json({ message: "Failed to fetch notes" });
+    res.status(500).json({ message: error.message });
   }
 };
